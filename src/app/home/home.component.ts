ba3e75d28984db9072750b7dfdf147241b9d9298
OkomoJacob <okomo.code@gmail.com>
2022-11-24 06:51:25 +0300

    Home componenet init :books:, :sparkles:

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { HomeServiceService } from './../home-service.service';
+import { Component, OnInit } from '@angular/core';
+import { Name } from '../home'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  myName: Name = {
+    name: 'Jacob'
+  }
+  constructor(private homeService: HomeServiceService) { }
+
+  ngOnInit(): void {
+  }
+
+}
