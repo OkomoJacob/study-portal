842b16c3376f58085abcb7c141b0316e08aa1ce2
okomojacob <okomo.code@gmail.com>
2023-02-10 11:55:43 +0300

    Create new student

--- src/app/student-create/student-create.component.ts
+++ src/app/student-create/student-create.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeServiceService } from '../services/home-service.service';
 
 @Component({
   selector: 'app-student-create',
   templateUrl: './student-create.component.html',
-  styleUrls: ['./student-create.component.css']
+  styleUrls: ['./student-create.component.css'],
 })
 export class StudentCreateComponent implements OnInit {
+  student: { id: any; name: string; description: string; email: string } = {
+    id: null,
+    name: '',
+    description: '',
+    email: '',
+  };
 
-  constructor() { }
+  constructor(public homeService: HomeServiceService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  
+  public createStudent() {
+    console.log('Student -->>', this.student);
+    this.homeService.createStudent(this.student);
+    this.student = { id: null, name: '', description: '', email: '' };
   }
-
 }
